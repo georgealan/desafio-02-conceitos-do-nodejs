1f57bf53e351e764d00c135117876a9531ada91f
George Alan <georgealanrufo@gmail.com>
2020-07-20 20:26:42 -0300

    desafio 02 finalizado

--- src/app.js
+++ src/app.js
@@ -7,32 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-/**
- * 
- * POST /repositories: A rota deve receber title, url e techs dentro do corpo da requisição, sendo a URL o link para o 
- * github desse repositório. Ao cadastrar um novo projeto, ele deve ser armazenado dentro de um objeto no 
- * seguinte formato: { id: "uuid", title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 }; 
- * Certifique-se que o ID seja um UUID, e de sempre iniciar os likes como 0. 
- * 
- * GET /repositories: Rota que lista todos os repositórios;
- * PUT /repositories/:id: A rota deve alterar apenas o title, a url e as techs do repositório que possua o id igual ao 
- * id presente nos parâmetros da rota;
- * DELETE /repositories/:id: A rota deve deletar o repositório com o id presente nos parâmetros da rota;
- * POST /repositories/:id/like: A rota deve aumentar o número de likes do repositório específico escolhido através do id
- * presente nos parâmetros da rota, a cada chamada dessa rota, o número de likes deve ser aumentado em 1;
- * 
- * Dica: Acima utilizamos POST em uma rota, mesmo ela alterando o número de likes do repositório sem criar nada diretamente.
- * Se dividirmos semânticamente as responsabilidades da nossa aplicação em entidades, o like seria uma entidade, e 
- * repository seria outra entidade. 
- * Com essa separação, temos diferentes regras de negócio para cada entidade, assim, ao chamar a rota de like e 
- * adicionamos apenas um like, podemos interpretar que estamos criando um novo like, e não atualizando os likes.
- * Então por que não usar PUT no lugar de POST? Justamente por estarmos "criando" UM novo like, e não atualizando o 
- * número de likes para qualquer outro valor.
- * Talvez fique difícil enxergar por ser apenas um número, mas pense que cada like seja salvo em uma tabela no banco 
- * junto do usuário que realizou esse like. Agora fica mais claro que você está criando um novo like, certo?
- */
-
-
 const repositories = [];
 
 function validateProjectId(request, response, next) {
@@ -44,7 +18,6 @@ function validateProjectId(request, response, next) {
 }
 
 app.use('/repositories/:id', validateProjectId);
-// app.use('/repositories/:id/like', validateProjectId, incrementLikes);
 
 app.get('/repositories', (request, response) => {
   const { title, techs } = request.query;
@@ -75,11 +48,14 @@ app.put("/repositories/:id", (request, response) => {
     return response.status(400).json({ error: 'Repository not found' });
   }
 
-  const repository = {
+  var repository = repositories[repositoryIndex];
+
+  repository = {
     id,
     title,
     url,
     techs,
+    likes: repository.likes,
   };
 
   repositories[repositoryIndex] = repository;
@@ -108,8 +84,11 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(400).json({ error: 'Repository not found' });
   }
 
+  var repository = repositories[repositoryIndex];
   
+  repository.likes++;
 
+  return response.json(repository);
 });
 
 module.exports = app;
